5848f0593644982421889d4ea11028a74d9e65c3
Abubakar-Ciise <abubakarciise4@gmail.com>
2024-07-30 17:06:20 +0300

    deploy

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
-  origin: ['https://social-media-wjk3.vercel.app'],
+  origin: ['https://social-media-iota-eight.vercel.app'],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
